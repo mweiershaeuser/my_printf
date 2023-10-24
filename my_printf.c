c19caf4b3f78ff104157957d3aacc8e1c27c8f1d
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-10-24 18:20:23 +0200

    fixed weird error reading va_args on arm by passing va_list pointer

--- my_printf.c
+++ my_printf.c
@@ -7,20 +7,20 @@
 #include <stdarg.h>
 #include "include/my.h"
 
-static int get_format(const char *format, va_list args)
+static int get_format(const char *format, va_list *args)
 {
     switch (*format) {
     case '%':
         my_putchar('%');
         return 1;
     case 'd':
-        return my_put_nbr(va_arg(args, int));
+        return my_put_nbr(va_arg(*args, int));
     case 'i':
-        return my_put_nbr(va_arg(args, int));
+        return my_put_nbr(va_arg(*args, int));
     case 's':
-        return my_putstr(va_arg(args, char *));
+        return my_putstr(va_arg(*args, char *));
     case 'c':
-        my_putchar(va_arg(args, int));
+        my_putchar(va_arg(*args, int));
         return 1;
     case '\0':
         return -1;
@@ -41,7 +41,7 @@ int my_printf(const char *format, ...)
     while (*format != '\0') {
         if (*format == '%') {
             format++;
-            res = get_format(format, args);
+            res = get_format(format, &args);
             cnt += res;
         } else {
             my_putchar(*format);
