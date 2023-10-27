0deb2d7614c96447cc3efe2858469f5432bd808d
Leticia <leticia.fraga@epitech.eu>
2023-10-27 17:42:52 +0200

    handle int

--- handle_int.c
+++ handle_int.c
@@ -8,6 +8,20 @@
 #include "include/my.h"
 #include "include/format.h"
 
+static char get_signal(int n, format_string *fs)
+{
+    char *plus = my_strstr(fs->flags, "+");
+    char *space = my_strstr(fs->flags, " ");
+
+    if (n < 0)
+        return '-';
+    if (plus)
+        return '+';
+    if (space)
+        return ' ';
+    return 0;
+}
+
 static void put_chars(int n, char c)
 {
     for (int i = 0; i < n; i++) {
@@ -15,14 +29,20 @@ static void put_chars(int n, char c)
     }
 }
 
-static int handle_width(int n, int num_len, format_string *fs)
+static int handle_width(int n, int num_len, format_string *fs, int signal)
 {
     char *right = my_strstr(fs->flags, "-");
     char *zero = my_strstr(fs->flags, "0");
-    char to_print = zero == 0 ? ' ' : '0';
 
-    if (right == 0) {
-        put_chars(fs->width - num_len, to_print);
+    if (signal != 0)
+        num_len += 1;
+    if (right == 0 && zero == 0) {
+        put_chars(fs->width - num_len, ' ');
+    }
+    if (signal != 0)
+        my_putchar(signal);
+    if (right == 0 && zero != 0) {
+        put_chars(fs->width - num_len, '0');
     }
     my_put_nbr(n);
     if (right != 0) {
@@ -34,11 +54,19 @@ static int handle_width(int n, int num_len, format_string *fs)
 int handle_int(va_list *args, format_string *fs)
 {
     int n = va_arg(*args, int);
-    int num_len = my_get_nbr_len(n);
+    int signal = get_signal(n, fs);
+    int num_len;
 
+    if (n < 0)
+        n = n * (-1);
+    num_len = my_get_nbr_len(n);
     if (num_len >= fs->width) {
+        if (signal != 0) {
+            my_putchar(signal);
+            num_len += 1;
+        }
         my_put_nbr(n);
         return num_len;
     } else
-        return handle_width(n, num_len, fs);
+        return handle_width(n, num_len, fs, signal);
 }
--- tests/test_handle_int.c
+++ tests/test_handle_int.c
@@ -14,7 +14,7 @@ Test (handle_int, basic_test, .init = cr_redirect_stdout)
     cr_assert_eq (cnt, 2);
 }
 
-Test (handle_int, handle_int_flags, .init = cr_redirect_stdout)
+Test (handle_int, handle_zero_minus, .init = cr_redirect_stdout)
 {
     int n = 42;
     int cnt = 0;
@@ -25,8 +25,41 @@ Test (handle_int, handle_int_flags, .init = cr_redirect_stdout)
     cnt += my_printf("%-010d", n);
     cnt += my_printf("%0-10d", n);
 
-    //receive_output("handle_int_flags_test.txt");
     cr_assert_stdout_eq_str(
         "        42000000004242        42        42        ");
     cr_assert_eq (cnt, 50);
 }
+
+Test (handle_int, handle_plus_space, .init = cr_redirect_stdout)
+{
+    int n = 42;
+    int cnt = 0;
+
+    cnt += my_printf("%     d", n);
+    cnt += my_printf("% 10d", n);
+    cnt += my_printf("% 010d", n);
+    cnt += my_printf("%+ -10d", n);
+    cnt += my_printf("%+ -010d", n);
+    cnt += my_printf("%+010d", n);
+    
+    cr_assert_stdout_eq_str(
+        " 42        42 000000042+42       +42       +000000042");
+    cr_assert_eq (cnt, 53);
+}
+
+Test (handle_int, handle_plus_space_neg, .init = cr_redirect_stdout)
+{
+    int n = -42;
+    int cnt = 0;
+
+    cnt += my_printf("%     d", n);
+    cnt += my_printf("% 10d", n);
+    cnt += my_printf("% 010d", n);
+    cnt += my_printf("%+ -10d", n);
+    cnt += my_printf("%+ -010d", n);
+    cnt += my_printf("%+010d", n);
+
+    cr_assert_stdout_eq_str(
+        "-42       -42-000000042-42       -42       -000000042");
+    cr_assert_eq (cnt, 53);
+}  
