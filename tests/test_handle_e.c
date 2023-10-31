3ecde53e00259cba5614ec7d639c527be09708dc
Melvin Weiershäuser <51531071+mweiershaeuser@users.noreply.github.com>
2023-10-31 15:32:46 +0100

    added test for e conversion for making coverage beautiful :D

--- tests/test_handle_e.c
+++ tests/test_handle_e.c
@@ -5,14 +5,14 @@
 
 Test(my_printf, scientific_notation_lower, .init = cr_redirect_stdout)
 {
-    my_printf("%e %e %e %e %e", 2.0, 14.46, -3.7, 0.78, -0.007);
+    my_printf("%e %e %e %e %e %e", 2.0, 14.46, -3.7, 0.78, -0.007, 12345678910.0);
     //receive_output("scientific_notation_lower_test.txt");
-    cr_assert_stdout_eq_str("2.000000e+00 1.446000e+01 -3.700000e+00 7.800000e-01 -7.000000e-03");
+    cr_assert_stdout_eq_str("2.000000e+00 1.446000e+01 -3.700000e+00 7.800000e-01 -7.000000e-03 1.234567e+10");
 }
 
 Test(my_printf, scientific_notation_upper, .init = cr_redirect_stdout)
 {
-    my_printf("%E %E %E %E %E", 2.0, 14.46, -3.7, 0.78, -0.007);
+    my_printf("%E %E %E %E %E %E", 2.0, 14.46, -3.7, 0.78, -0.007, 12345678910.0);
     //receive_output("scientific_notation_upper_test.txt");
-    cr_assert_stdout_eq_str("2.000000E+00 1.446000E+01 -3.700000E+00 7.800000E-01 -7.000000E-03");
+    cr_assert_stdout_eq_str("2.000000E+00 1.446000E+01 -3.700000E+00 7.800000E-01 -7.000000E-03 1.234567E+10");
 }
